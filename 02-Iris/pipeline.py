040e2ff5bf0db549b29d2b8f78f6576308b9bbf2
nevret <nevret@naver.com>
2024-02-26 17:38:26 +0900

    Update source code

--- 02-Iris/pipeline.py
+++ 02-Iris/pipeline.py
@@ -1,20 +1,76 @@
+import kfp
 from kfp import dsl
-import kfp.components as comp
 
-from logistic_regression.logistic_regression import *
 
 @dsl.pipeline(
-    name='test',
-    description='test'
+    name='nevret-iris',
+    description='nevret iris test'
 )
-def test_pipeline():
-    logistic_regression_task = logistic_regression()
-    return logistic_regression_task.output
+def iris_pipeline():
+    add_p = dsl.ContainerOp(
+        name="load iris data pipeline",
+        image="nevret/nevret-iris-preprocessing:0.5",
+        command=['python', 'load_data.py'],
+        arguments=[
+            '--data_path', './Iris.csv'
+        ],
+        file_outputs={'iris' : '/iris.csv'}
+    )
     
+    ml = dsl.ContainerOp(
+        name="training pipeline",
+        image="nevret/nevret-iris-training:0.5",
+        command=['python', 'train.py'],
+        arguments=[
+            '--data', add_p.outputs['iris']
+        ]
+    )
+
+    ml.after(add_p)
+    
+    
+    
+if __name__ == "__main__":
+    import kfp.compiler as compiler
+    compiler.Compiler().compile(iris_pipeline, "pipeline.yaml")
+    
+    import kfp
+    import requests
+
+    USERNAME = "jklee@euclidsoft.co.kr"
+    PASSWORD = "dbzmfflem1!"
+    NAMESPACE = "addaugmentation"
+    HOST = "https://kubeflow.euso.kr" # istio-ingressgateway's external-ip created by the load balancer.
+
+    session = requests.Session()
+    response = session.get(HOST)
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    data = {"login": USERNAME, "password": PASSWORD}
+    session.post(response.url, headers=headers, data=data)
+    session_cookie = session.cookies.get_dict()["authservice_session"]    
 
     
+    # Submit the pipeline for execution
+    pipeline_func = iris_pipeline
+    client = kfp.Client(
+        host=f"{HOST}/pipeline",
+        namespace=f"{NAMESPACE}",
+        cookies=f"authservice_session={session_cookie}",
+    )
+    print(client.list_pipelines())
     
-if __name__ == '__main__':
-    import kfp.compiler as compiler
-    compiler.Compiler().compile(test_pipeline, __file__ + "pipeline.yaml")
+    run = client.create_run_from_pipeline_func(pipeline_func, arguments={})
+    
+    print('')
+    '''
+    1. docker image 생성 (바로 tag도 달아서)
+    docker build -t nevret/nevret-iris-preprocessing:0.5 .
+    
+    2. dockerhub에 배포
+    docker push nevret/nevret-iris-preprocessing:0.5
+    '''
     
